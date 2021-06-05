d0a980a3329c36bb9d0d4f26e988ee2a48420f0f
pavas <pavas@zivitech.com>
2021-06-05 17:32:05 +0300

    fix price_zakupka field add filter by empty price_zakupka field (by 0)

--- src/extractors/belLavka/index.js
+++ src/extractors/belLavka/index.js
@@ -140,7 +140,6 @@ const prepareDataForMilModa = items => items.map((item) => {
     fabric_txt,
     description,
     name,
-    retail_price,
   } = item
 
   return {
@@ -152,7 +151,7 @@ const prepareDataForMilModa = items => items.map((item) => {
     },
     cat_nazv: category.value,
     height: Object.keys(heights).map(height => heights[height].value).join("-").trim(),
-    price_zakupka: buy_price || retail_price,
+    price_zakupka: buy_price,
     size_list: Object.keys(sizes).map(size => sizes[size].value),
     sostav: fabric_txt,
     text: description,
@@ -225,7 +224,9 @@ const parsingByBrand = async (brandInfo, page, parsingDate) => {
 
   console.log(`Фильтруем спаршеные товары по дате (дата ${parsingDate}), было ${allInfoAboutItems.length}`)
 
-  const filteredItems = allInfoAboutItems.filter(({ updated_date }) => new Date(parsingDate) <= new Date(updated_date))
+  const filteredItems = allInfoAboutItems
+    .filter(({ updated_date }) => new Date(parsingDate) <= new Date(updated_date))
+    .filter(({ price_zakupka }) => !!price_zakupka)
 
   console.log(`стало ${filteredItems.length}`)
 
